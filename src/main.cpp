02655921e64c21d5d20872000ff9bd7eb05a9bd0
Hugo Charles <hugo.y.charles@gmail.com>
2019-09-22 14:54:04 +0200

    Add comments before each bikes
    This should avoid conflicts when setting a name to bike

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,13 @@
 int main() {
   Road road(150);
 
+  // First student must give a name to bike
   Bike bike("");
+
+  // Second student must give a name to bike2
   Bike bike2("");
+
+  // Third student (if any) must give a name to bike3
   Bike bike3("");
 
   Race race(road, std::vector<Bike>{bike, bike2, bike3});
